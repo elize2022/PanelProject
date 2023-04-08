b789a6f6408bd198f6176629cee5788e8e56eff9
elize2022 <elizaveta020281@gmail.com>
2023-04-08 16:37:26 +0300

    Add project files.

---
+++ PanelProject/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PanelProject
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            Panel1 panel1Window = new Panel1();
+            panel1Window.Owner = this;
+            panel1Window.WindowStartupLocation = WindowStartupLocation.CenterOwner;
+            panel1Window.Show();
+        }
+
+        private void button2_Click(object sender, RoutedEventArgs e)
+        {
+            Panel2 panel2Window = new Panel2();
+            panel2Window.Owner = this;
+            panel2Window.WindowStartupLocation = WindowStartupLocation.CenterOwner;
+            panel2Window.Show();
+        }
+
+        private void button3_Click(object sender, RoutedEventArgs e)
+        {
+            Panel3 panel3Window = new Panel3();
+            panel3Window.Owner = this;
+            panel3Window.WindowStartupLocation = WindowStartupLocation.CenterOwner;
+            panel3Window.Show();
+        }
+    }
+}
---
+++ PanelProject/Panel2.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace PanelProject
+{
+    /// <summary>
+    /// Interaction logic for Panel2.xaml
+    /// </summary>
+    public partial class Panel2 : Window
+    {
+        public Panel2()
+        {
+            InitializeComponent();
+        }
+    }
+}
